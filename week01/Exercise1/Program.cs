0460d938a9dbf7979337f4b7c88bd6196d5fa8ba
rieperrebeca <rsilva11@byupathway.edu>
2026-09-08 22:00:31 -0300

    update

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,13 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-    }
-    
-        Console.WriteLine("Enter your first name: ");
+
+        Console.Write("Enter your first name: ");
     string firstName = Console.ReadLine();
 
-        Console.WriteLine("Enter your last name: ");
+    Console.Write("Enter your last name: ");
     string lastName = Console.ReadLine();
     Console.WriteLine($"Your name is {lastName}, {firstName}");
+    }
+    
 
 }
