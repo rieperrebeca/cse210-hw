f13850e9829fcbbaedc665fef976b53a5bb0b8e0
rieperrebeca <rsilva11@byupathway.edu>
2026-09-10 21:15:26 -0300

    update

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        string grade;
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You can do better next time.");
+        }
     }
 }
