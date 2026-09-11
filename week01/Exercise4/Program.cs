0fde18bf4959a4cbf0b33534983b69be1f41f54d
rieperrebeca <rsilva11@byupathway.edu>
2026-09-10 21:34:01 -0300

    update

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -6,55 +6,45 @@ class Program
     static void Main(string[] args)
     {
         List<int> numbers = new List<int>();
-        
-        // Please note we could use a do-while loop here instead
-        int userNumber = -1;
-        while (userNumber != 0)
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+
+        while (number != 0)
         {
-            Console.Write("Enter a number (0 to quit): ");
-            
-            string userResponse = Console.ReadLine();
-            userNumber = int.Parse(userResponse);
-            
-            // Only add the number to the list if it is not 0
-            if (userNumber != 0)
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
             {
-                numbers.Add(userNumber);
+                numbers.Add(number);
             }
         }
 
-        // Part 1: Compute the sum
         int sum = 0;
-        foreach (int number in numbers)
+
+        foreach (int item in numbers)
         {
-            sum += number;
+            sum += item;
         }
 
         Console.WriteLine($"The sum is: {sum}");
 
-        // Part 2: Compute the average
-        // Notice that we first cast the sum variable to be a float. Otherwise, because
-        // both the sum and the count are integers, the computer will do integer division
-        // and I will not get a decimal value (even though it puts the result into a float variable).
+        float average = (float)sum / numbers.Count;
 
-        // By making one of the variables a float first, the computer knows that it has to
-        // do the floating point division, and we get the decimal value that we expect.
-        float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
 
-        // Part 3: Find the max
-        // There are several ways to do this, such as sorting the list
-        
-        int max = numbers[0];
+        int largest = numbers[0];
 
-        foreach (int number in numbers)
+        foreach (int item in numbers)
         {
-            if (number > max)
+            if (item > largest)
             {
-                // if this number is greater than the max, we have found the new max!
-                max = number;
+                largest = item;
             }
         }
 
-        Console.WriteLine($"The max is: {max}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
+}
