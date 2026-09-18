22670cdc20ab23e88effa500281ff6f865fc47d6
rieperrebeca <rsilva11@byupathway.edu>
2026-09-17 22:23:00 -0300

    update

---
+++ week03/scripture.cs
@@ -0,0 +1,57 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public string GetDisplayText()
+{
+    string display = _reference.GetDisplayText() + "\n";
+
+    foreach (Word word in _words)
+    {
+        display += word.GetDisplayText() + " ";
+    }
+
+    return display;
+}
+
+
+public void HideRandomWords(int numberToHide)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < numberToHide; i++)
+    {
+        int index = random.Next(_words.Count);
+
+        _words[index].Hide();
+    }
+}
+
+public bool IsCompletelyHidden()
+{
+    foreach (Word word in _words)
+    {
+        if (!word.IsHidden())
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+}
