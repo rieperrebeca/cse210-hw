115aab7d83a65752671c9463011f8154a2876efc
rieperrebeca <rsilva11@byupathway.edu>
2026-09-17 22:32:45 -0300

    UPDATE

--- week03/scripture.cs
+++ week03/scripture.cs
@@ -1,57 +1,19 @@
-public class Scripture
-{
-    private Reference _reference;
-    private List<Word> _words;
-
-    public Scripture(Reference reference, string text)
-    {
-        _reference = reference;
-        _words = new List<Word>();
-
-        string[] words = text.Split(' ');
-
-        foreach (string word in words)
-        {
-            _words.Add(new Word(word));
-        }
-    }
-
-    public string GetDisplayText()
-{
-    string display = _reference.GetDisplayText() + "\n";
-
-    foreach (Word word in _words)
-    {
-        display += word.GetDisplayText() + " ";
-    }
-
-    return display;
-}
-
-
 public void HideRandomWords(int numberToHide)
 {
     Random random = new Random();
+    int wordsHidden = 0;
 
-    for (int i = 0; i < numberToHide; i++)
+    // The !IsCompletelyHidden() check prevents an infinite loop 
+    // when there are fewer unhidden words left than numberToHide.
+    while (wordsHidden < numberToHide && !IsCompletelyHidden())
     {
         int index = random.Next(_words.Count);
-
-        _words[index].Hide();
-    }
-}
-
-public bool IsCompletelyHidden()
-{
-    foreach (Word word in _words)
-    {
-        if (!word.IsHidden())
+        
+        // Only hide and count it if it is currently visible
+        if (!_words[index].IsHidden())
         {
-            return false;
+            _words[index].Hide();
+            wordsHidden++;
         }
     }
-
-    return true;
-}
-
 }
