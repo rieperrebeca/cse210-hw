7c6a2fe560de2c11807b863bf7a1a6ae5e7844cd
rieperrebeca <rsilva11@byupathway.edu>
2026-09-17 22:39:12 -0300

    update scripture

--- week03/scripture.cs
+++ week03/scripture.cs
@@ -1,17 +1,64 @@
-public void HideRandomWords(int numberToHide)
-{
-    Random random = new Random();
-    int wordsHidden = 0;
+using System;
+using System.Collections.Generic;
 
-    while (wordsHidden < numberToHide && !IsCompletelyHidden())
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
     {
-        int index = random.Next(_words.Count);
-        
-        // Only hide and count it if it is currently visible
-        if (!_words[index].IsHidden())
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
         {
-            _words[index].Hide();
-            wordsHidden++;
+            _words.Add(new Word(word));
         }
+    }
+
+    public string GetDisplayText()
+    {
+        string display = _reference.GetDisplayText() + "\n";
+
+        foreach (Word word in _words)
+        {
+            display += word.GetDisplayText() + " ";
+        }
+
+        return display;
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        int wordsHidden = 0;
+
+        while (wordsHidden < numberToHide && !IsCompletelyHidden())
+        {
+            int index = random.Next(_words.Count);
+            
+            // Only hide and count it if it is currently visible
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                wordsHidden++;
+            }
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
